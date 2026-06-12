96ee04d33e989cded03894f5610d856d9ec72d04
PARAS AHUJA <ahujap315@gmail.com>
2026-06-12 18:35:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+// alert(3+3); -- we are using nodejs not browser
+// console.log(3+3) console.log("PARAS") -- this will give error because of missing semicolon, but it is not recommended to use semicolons in JavaScript. It is better to use newlines to separate statements.
+console.log(3+3)
+console.log("PARAS") 
+let name = "hietsh" // string datattype
+let age = 23; // number datatype
+let isApproved = true; // boolean datatype
+let firstName = undefined; // undefined datatype
+let selectedColor = null;//standalone value
+//the range of number is 2 to power 53, but we can use BigInt for larger numbers
+//bigint
+//string - can we used in single quote or double quote
+//undefined means abhi value assign nii hui and null mtlb value empty hnn
+//symbol - used to create unique identifiers for objects
+
+//objects - used to store collections of data and more complex entities
+console.log(typeof "hitesh");//to get datatype of a variable
+console.log(typeof age);
+console.log(typeof undefined);//undefined
+console.log(typeof null); // this will give object, but it is a bug in JavaScript, it should be null    
