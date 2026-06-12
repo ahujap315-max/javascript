3cc48ad31984a3664a300104ce6fe44edca63455
PARAS AHUJA <ahujap315@gmail.com>
2026-06-12 18:10:37 +0000

    variables code added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid = 144553;
+let email = "ahujap315";
+var pass = "12345";
+/* prefer not to use var, as it is function scoped and can lead to unexpected behavior. Use let for variables that may change and const for variables that should not change. */
+city = "jaipur";
+let state;
+//accountid = 2; -- not allowed
+email = "hc@hc";
+pass = "123";
+city = "morada";
+console.log(email); 
+console.table([email, pass, city , state]); 
+console.log(accountid);
