8832b58c9a6332626c3ed06ad3a17afad61a2354
Paras Ahuja <ahujap315@gmail.com>
2026-06-21 21:42:37 +0530

    string commit

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "hitesh";
+const repocount = 50;
+console.log(name + repocount + " value");
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+const gamename = new String(`paras-ahuja`);
+console.log(gamename[0]);
+console.log(gamename.__proto__);//to see objects
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('a'));
+const newstr = gamename.substring(0 , 4);//last value will not be included
+console.log(newstr);
+
+//to see different string methods go to chrome click inspect --->const gamename = new String(`paras`);and see all the string functions
+//use mdn reference to understand 
+
